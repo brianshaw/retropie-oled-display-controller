eff6e181748a6ff9a374fede94f102c659296ec0
Brian Shaw <brian@beclicking.com>
2024-07-22 12:28:31 -0500

    test

---
+++ linux/retropie_oled_display_controller/main.c
@@ -0,0 +1,144 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : Robert Abram, Katie Snow
+ Version     :
+ Copyright   : Copyright 2014 Robert Abram, Katie Snow
+ Description : Ultimarc command line utility
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <json.h> # https://github.com/katie-snow/Ultimarc-linux/blob/master/src/libs/pacdrive.c
+
+typedef struct args
+{
+  int help;
+  int multi;
+} args;
+
+int
+main (int argc, char **argv)
+{
+  int idx;
+  int retVal;
+
+  // ulboard board;
+
+  args args;
+  args.help = 0;
+  args.multi = 0;
+
+  if (argc == 1)
+  {
+    args.help = 1;
+  }
+
+  for (idx = 1; idx < argc; ++idx)
+  {
+    if (strcmp (argv[idx], "-h") == 0 || strcmp (argv[idx], "--help") == 0)
+      args.help = 1;
+    if (strcmp (argv[idx], "-m") == 0 || strcmp (argv[idx], "--multi") == 0)
+      args.multi = 1;
+  }
+
+  if (args.help)
+  {
+    printf ("umtool [-h] [--help] [-m config file] [--multi config file] [config files...]\n");
+    printf ("-h | --help\t Prints this information\n");
+    printf ("-m | --multi\t File provided has multiple configuration\n");
+    printf ("config files\t JSON Configuration files to be processed\n");
+    retVal = EXIT_SUCCESS;
+    goto exit;
+  }
+
+  // if (args.multi)
+  // {
+  //   printf ("Loading multiple configurations from file %s...\n", argv[2]);
+  //   ulMultiConfigurationsFileStr(argv[2]);
+  //   retVal = EXIT_SUCCESS;
+  //   goto exit;
+  // }
+
+  // for (idx = 1; idx < argc; ++idx)
+  // {
+  //   printf ("Loading %s...\n", argv[idx]);
+  //   retVal = ulValidateConfigFileStr (argv[idx], &board);
+
+  //   if (retVal == 0)
+  //   {
+  //     // retVal = ulWriteToBoardFileStr(argv[idx], &board);
+  //   }
+  // }
+
+  loadGameConfig();
+
+  exit: return retVal;
+}
+
+int
+ulValidateConfig (json_object* bcfg, ulboard* ulcfg)
+{
+  int retCode = 0;
+
+  if (bcfg && ulcfg)
+  {
+    // retCode = ulGetProdAndVersion (bcfg, ulcfg);
+    
+    // if (retCode == 0)
+    // {
+    //   if (isIPACConfig (bcfg, ulcfg)
+    //       || isIPACUltimateConfig (bcfg, ulcfg)
+    //       || isPACDriveConfig (bcfg, ulcfg)
+    //       || isPACLED64Config (bcfg, ulcfg)
+    //       || isUltraStikConfig (bcfg, ulcfg)
+    //       || isUSBButtonConfig(bcfg, ulcfg)
+    //       || isServoStikConfig(bcfg, ulcfg)
+    //       || isUHidConfig (bcfg, ulcfg))
+    //   {
+    //     log_info("Configuration is %s. [Validated]", ulBoardTypeToString(ulcfg->type));
+    //   }
+    //   else
+    //   {
+    //     retCode = -1;
+    //     log_err("Configuration is '%s'. [Not validated].", ulBoardTypeToString(ulcfg->type));
+    //   }
+    // }
+    // else
+    // {
+    //   retCode = -1;
+    //   log_err("Configuration is '%s'. [Not validated].", ulBoardTypeToString(ulcfg->type));
+    // }
+  }
+  else
+  {
+    retCode = -1;
+    log_err("JSON format invalid");
+  }
+
+  return retCode;
+}
+
+int
+ulValidateConfigFileStr (const char* file, ulboard* board)
+{
+  json_object *bcfg = json_object_from_file (file);
+
+  return ulValidateConfig (bcfg, board);
+}
+
+int
+loadGameConfig()
+{
+  printf ("Loading %s...\n", argv[1]);
+  retVal = ulValidateConfigFileStr (argv[1], &board);
+
+  if (retVal == 0)
+  {
+    // retVal = ulWriteToBoardFileStr(argv[idx], &board);
+  }
+
+  return 0;
+}
---
+++ linux/test_display_contoller.c
@@ -0,0 +1,198 @@
+#include <stdio.h>
+#include <string.h>
+
+/* Unix */
+#include <json.h>
+
+bool isGameConfig(json_object* jobj)
+{
+  isBoardCfg = validateGameConfigData(jobj);
+
+  return isBoardCfg;
+}
+
+bool validateGameConfigData(json_object* jobj)
+{
+	bool valid = true;
+
+	// int idx = 0;
+
+	// json_object* leds = NULL;
+	// json_object* led = NULL;
+
+	// if (checkBoardID(jobj, "board id"))
+	// {
+	// 	if (json_object_object_get_ex(jobj, "led", &leds))
+	// 	{
+	// 		if (json_object_get_type(leds) == json_type_array)
+	// 		{
+	// 			for (idx = 0; idx < json_object_array_length(leds); ++ idx)
+	// 			{
+	// 				led = json_object_array_get_idx(leds, idx);
+	// 				if (json_object_get_type(led) != json_type_int)
+	// 				{
+	// 					log_err ("Type invalid.  Valid type is integer");
+	// 					valid = false;
+	// 				}
+	// 			}
+	// 		}
+	// 		else
+	// 		{
+	// 			log_err ("'led' is not defined as an array");
+	// 			valid = false;
+	// 		}
+	// 	}
+	// 	else
+	// 	{
+	// 		log_err ("Missing required entry.  Valid entry is 'led'");
+	// 		valid = false;
+	// 	}
+	// }
+	return valid;
+}
+
+
+
+
+
+//
+// multi oled test program
+// Written by Larry Bank
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "ss_oled.h"
+#include <sys/inotify.h>
+#include <json.h> # https://github.com/katie-snow/Ultimarc-linux/blob/master/src/libs/pacdrive.c
+
+#define EVENT_SIZE (sizeof(struct inotify_event)) // Add this line to declare the missing variable
+#define BUF_LEN (1024 * (EVENT_SIZE + 16)) // Add this line to declare the missing variable
+
+SSOLED ssoled[2]; // data structure for 2 OLED objects
+unsigned char ucBackBuf[1024];
+
+char* folderToWatch = "displayTexts";
+int length, ifile = 0;
+int fd;
+int wd;
+char buffer[BUF_LEN];
+
+
+void updateDisplay(); // Add this line to provide a function prototype
+void watchDisplayUpdate(); // Add this line to provide a function prototype
+
+int main(int argc, char *argv[])
+{
+  fprintf(stdout, "main");
+int i;
+int iOLEDAddr = -1; // typical address; it can also be 0x3d
+int iOLEDType0 = OLED_128x64; // Change this for your specific display
+int iOLEDType1 = OLED_64x32;
+int bFlip = 0, bInvert = 0, bWire = 1;
+
+
+
+    watchDisplayUpdate();
+
+
+
+// For hardware I2C on the RPI, the clock rate is fixed and set in the
+// /boot/config.txt file, so we pass 0 for the bus speed
+    // i=oledInit(&ssoled[0], iOLEDType0, iOLEDAddr, bFlip, bInvert, bWire, 1, -1, -1, 0); // initialize 128x64 oled on I2C channel 1
+    i=oledInit(&ssoled[0], iOLEDType0, iOLEDAddr, bFlip, bInvert, bWire, 4, 9, 8, 0); // initialize 128x64 oled on I2C channel 1
+  if (i != OLED_NOT_FOUND)
+  {
+                char *msgs[] = {(char *)"SSD1306 @ 0x3C", (char *)"SSD1306 @ 0x3D",(char *)"SH1106 @ 0x3C",(char *)"SH1106 @ 0x3D"};
+    printf("Successfully opened I2C bus 1\n");
+                oledSetBackBuffer(&ssoled[0], ucBackBuf);
+    oledFill(&ssoled[0], 0,1); // fill with black
+    oledWriteString(&ssoled[0], 0,0,0, msgs[i], FONT_NORMAL,0,1);
+    oledWriteString(&ssoled[0], 0,0,1,"SS_OLED Library!",FONT_NORMAL,0,1);
+    oledWriteString(&ssoled[0], 0,3,2,"BIG!",FONT_LARGE,0,1);
+    oledWriteString(&ssoled[0], 0,0,5,"Small", FONT_SMALL,0,1);
+    // oledWriteString(&ssoled[0], 0,0,6,argv[1], FONT_SMALL,0,1);
+    for (i=0; i<64; i++)
+    {
+      oledSetPixel(&ssoled[0], i, 16+i, 1, 1);
+      oledSetPixel(&ssoled[0], 127-i, 16+i, 1, 1);
+    }
+    
+    oledInit(&ssoled[1], iOLEDType1, iOLEDAddr, bFlip, bInvert, bWire, 6, -1, -1, 0); // initialize the 64x32 oled on I2C channel 6
+    oledFill(&ssoled[1], 0, 1);
+    oledWriteString(&ssoled[1], 0,0,0,"OLED on", FONT_NORMAL, 0, 1);
+    oledWriteString(&ssoled[1], 0,0,1,"I2C ch 6", FONT_NORMAL, 0, 1);
+    
+    printf("Press ENTER to quit\n");
+    
+    getchar();
+    oledPower(&ssoled[0], 0); // turn off both displays
+    oledPower(&ssoled[1], 0);
+
+    (void) inotify_rm_watch(fd, wd);
+    (void) close(fd);
+  }
+  else
+  {
+    printf("Unable to initialize I2C bus 0-2, please check your connections and verify the device address by typing 'i2cdetect -y <channel>\n");
+  }
+
+  
+   return 0;
+} /* main() */
+
+// void updateDisplay() {
+//   FILE *file = fopen(fileToWatch, "r");
+//   if (file == NULL) {
+//     perror("fopen");
+//     exit(EXIT_FAILURE);
+//   }
+
+//   char line[256];
+//   while (fgets(line, sizeof(line), file)) {
+//     // Process each line of the file
+//     // ...
+//     // oledWriteString(&ssoled[0], 0,0,6,line, FONT_SMALL,0,1);
+//     printf("%s", line);
+//   }
+
+//   fclose(file);
+// }
+
+void watchDisplayUpdate() {
+    printf("Watching \n");
+    fd = inotify_init();
+
+    if (fd < 0) {
+        perror("inotify_init");
+    }
+
+    // wd = inotify_add_watch(fd, ".",
+    wd = inotify_add_watch(fd, folderToWatch,
+        IN_MODIFY | IN_CREATE | IN_DELETE);
+    length = read(fd, buffer, BUF_LEN);
+
+    if (length < 0) {
+        perror("read");
+    }
+
+    while (ifile < length) {
+        struct inotify_event *event =
+            (struct inotify_event *) &buffer[ifile];
+        if (event->len) {
+            if (event->mask & IN_CREATE) {
+                printf("The file %s was created.\n", event->name);
+            } else if (event->mask & IN_DELETE) {
+                printf("The file %s was deleted.\n", event->name);
+            } else if (event->mask & IN_MODIFY) {
+                printf("The file %s was modified.\n", event->name);
+            }
+            watchDisplayUpdate();
+        }
+        ifile += EVENT_SIZE + event->len;
+    }
+
+    (void) inotify_rm_watch(fd, wd);
+    (void) close(fd);
+}
