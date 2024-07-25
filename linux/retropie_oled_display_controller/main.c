f202af4e57f8facc9fc744e463c66a99a712f76e
Brian Shaw <brian@beclicking.com>
2024-07-25 08:45:48 -0500

    test

--- linux/retropie_oled_display_controller/main.c
+++ linux/retropie_oled_display_controller/main.c
@@ -62,15 +62,15 @@ main (int argc, char **argv)
   sigemptyset (&new_action.sa_mask);
   new_action.sa_flags = 0;
 
-  sigaction (SIGINT, NULL, &old_action);
-  if (old_action.sa_handler != SIG_IGN)
-    sigaction (SIGINT, &new_action, NULL);
-  sigaction (SIGHUP, NULL, &old_action);
-  if (old_action.sa_handler != SIG_IGN)
-    sigaction (SIGHUP, &new_action, NULL);
-  sigaction (SIGTERM, NULL, &old_action);
-  if (old_action.sa_handler != SIG_IGN)
-    sigaction (SIGTERM, &new_action, NULL);
+  // sigaction (SIGINT, NULL, &old_action);
+  // if (old_action.sa_handler != SIG_IGN)
+  //   sigaction (SIGINT, &new_action, NULL);
+  // sigaction (SIGHUP, NULL, &old_action);
+  // if (old_action.sa_handler != SIG_IGN)
+  //   sigaction (SIGHUP, &new_action, NULL);
+  // sigaction (SIGTERM, NULL, &old_action);
+  // if (old_action.sa_handler != SIG_IGN)
+  //   sigaction (SIGTERM, &new_action, NULL);
 
   int idx;
   
