86ca76cabd0f1b43e12fa5dd3ddc81bb33f83c3a
Brian Shaw <brian@beclicking.com>
2024-07-29 14:32:50 -0500

    test

--- linux/retropie_oled_display_controller/main.c
+++ linux/retropie_oled_display_controller/main.c
@@ -228,6 +228,9 @@ loadGameConfig()
         if (json_object_object_get_ex(bcfg, "P1_BUTTON1", &tmp)) {
           if (json_object_get_string(tmp)) {
             oledWriteString(&ssoled[0], 0,0,5, (char*)json_object_get_string(tmp), FONT_SMALL,0,1);
+          } else {
+            // oledPower(&ssoled[0], 0);
+            buttonAinitated = -1;
           }
         } else {
           // oledPower(&ssoled[0], 0);
@@ -236,6 +239,9 @@ loadGameConfig()
         if (json_object_object_get_ex(bcfg, "P1_BUTTON2", &tmp)) {
           if (json_object_get_string(tmp)) {
             oledWriteString(&ssoled[1], 0,0,5, (char*)json_object_get_string(tmp), FONT_SMALL,0,1);
+          } else {
+            // oledPower(&ssoled[1], 0);
+            buttonBinitated = -1;
           }
         } else {
           // oledPower(&ssoled[1], 0);
